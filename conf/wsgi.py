69252f89be0501b496982b8c7b1112c9181699c3
Ben McAlindin <bmcalindin@gmail.com>
2016-11-12 15:00:53 +0000

    Moving wsgi to conf dir.

--- conf/wsgi.py
+++ conf/wsgi.py
@@ -4,6 +4,7 @@ import sys
 
 root = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(0, root)
+sys.path.insert(0, os.path.join(root, '..'))
 
 from app_factory import create_app
 application = create_app('DEV')
